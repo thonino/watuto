890a3d1c04937081c9d6a8093b82168531a3c800
thonino <thoninoben@gmail.com>
2023-12-09 02:07:30 +0100

    3em commit

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <div> <h1 className="text-center text-green-500">Home</h1>
+    <div> 
+      <h1 className="text-center text-green-500">Home</h1>
+      <h1 className="text-center text-red-500">Title</h1>
     </div>
   )
 }
